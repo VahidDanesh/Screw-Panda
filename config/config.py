8fc2e2514be3bb8573a8e08eaef635983ff9b796
Vahid <vahid.danesh@stonybrook.edu>
2025-02-10 15:14:14 -0500

    using genesis and swift

--- config/config.py
+++ config/config.py
@@ -4,4 +4,4 @@ Global configuration parameters
 
 # Simulation parameters
 PANDA_URDF = 'models/franka_description/robots/frankaEmikaPanda.urdf'
-PANDA_VIRTUAL = 'models/franka_description/robots/frankaEmikaPanda_virtual.urdf'
+PANDA_VIRTUAL = 'models/franka_description/robots/frankaEmikaPandaVirtual.urdf'
