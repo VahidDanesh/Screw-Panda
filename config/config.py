df62301cf574593f2785cfacd560e549b20e0bc0
vahid <vahid.danesh@stonybrook.edu>
2025-02-10 01:32:12 -0500

    initial upload

---
+++ config/config.py
@@ -0,0 +1,7 @@
+"""
+Global configuration parameters 
+"""
+
+# Simulation parameters
+PANDA_URDF = 'models/franka_description/robots/frankaEmikaPanda.urdf'
+PANDA_VIRTUAL = 'models/franka_description/robots/frankaEmikaPanda_virtual.urdf'
